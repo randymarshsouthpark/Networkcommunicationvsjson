f1aefc39d0a181bb09d98c543298bd4a7aa8ba0a
George Crick <George Crick>
2017-03-27 16:52:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2cricg55/networkcommunicationvsjson/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.a2cricg55.networkcommunicationvsjson;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+    public class MainActivity extends Activity implements View.OnClickListener {
+
+        class MyTask extends AsyncTask<Void, Void, String> {
+            public String doInBackground(Void... unnused) {
+                HttpURLConnection conn = null;
+                try {
+                    URL url = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php?artist=Oasis");
+                    conn = (HttpURLConnection) url.openConnection();
+                    InputStream in = conn.getInputStream();
+                    if (conn.getResponseCode() == 200) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                        String result = "", line;
+                        while ((line = br.readLine()) != null) result += line;
+                        return result;
+                    } else
+                        return "HTTP ERROR: " + conn.getResponseCode();
+
+                } catch (IOException e) {
+                    return e.toString();
+                } finally {
+                    if (conn != null)
+                        conn.disconnect();
+                }
+            }
+
+            public void onPostExecute(String result) {
+                EditText daedittext = (EditText) findViewById(R.id.daedittexter);
+                String artist = daedittext.getText().toString();
+                daedittext.setText(result);
+            }
+
+
+        }
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                Button newSong = (Button) findViewById(R.id.thebutton);
+                newSong.setOnClickListener(this);
+
+            }
+        public void onClick(View v) {
+
+
+            MyTask t = new MyTask();
+            t.execute();
+        }
+        }
