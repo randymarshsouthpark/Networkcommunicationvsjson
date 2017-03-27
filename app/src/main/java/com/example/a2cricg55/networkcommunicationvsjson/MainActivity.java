c7280c6a48257a51a014a8d87dabb15b111d76df
randymarshsouthpark <2cricg55@solent.ac.uk>
2017-03-27 16:55:47 +0100

    Updated

--- app/src/main/java/com/example/a2cricg55/networkcommunicationvsjson/MainActivity.java
+++ app/src/main/java/com/example/a2cricg55/networkcommunicationvsjson/MainActivity.java
@@ -16,53 +16,53 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
-    public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-        class MyTask extends AsyncTask<Void, Void, String> {
-            public String doInBackground(Void... unnused) {
-                HttpURLConnection conn = null;
-                try {
-                    URL url = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php?artist=Oasis");
-                    conn = (HttpURLConnection) url.openConnection();
-                    InputStream in = conn.getInputStream();
-                    if (conn.getResponseCode() == 200) {
-                        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-                        String result = "", line;
-                        while ((line = br.readLine()) != null) result += line;
-                        return result;
-                    } else
-                        return "HTTP ERROR: " + conn.getResponseCode();
+    class MyTask extends AsyncTask<String, Void, String> {
+        public String doInBackground(String... arguments) {
+            HttpURLConnection conn = null;
+            try {
+                URL url = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php?artist="+arguments[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStream in = conn.getInputStream();
+                if (conn.getResponseCode() == 200) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String result = "", line;
+                    while ((line = br.readLine()) != null) result += line;
+                    return result;
+                } else
+                    return "HTTP ERROR: " + conn.getResponseCode();
 
-                } catch (IOException e) {
-                    return e.toString();
-                } finally {
-                    if (conn != null)
-                        conn.disconnect();
-                }
+            } catch (IOException e) {
+                return e.toString();
+            } finally {
+                if (conn != null)
+                    conn.disconnect();
             }
-
-            public void onPostExecute(String result) {
-                EditText daedittext = (EditText) findViewById(R.id.daedittexter);
-                String artist = daedittext.getText().toString();
-                daedittext.setText(result);
-            }
-
-
         }
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-
-                Button newSong = (Button) findViewById(R.id.thebutton);
-                newSong.setOnClickListener(this);
-
-            }
-        public void onClick(View v) {
-
-
-            MyTask t = new MyTask();
-            t.execute();
+        public void onPostExecute(String result) {
+            EditText daedittext = (EditText) findViewById(R.id.daedittexter);
+            String artist = daedittext.getText().toString();
+            daedittext.setText(result);
         }
-        }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button newSong = (Button) findViewById(R.id.thebutton);
+        newSong.setOnClickListener(this);
+
+    }
+    public void onClick(View v) {
+
+
+        MyTask t = new MyTask();
+        t.execute();
+    }
+}
